10e7c5bedcc724bcf468628a7d422ad5f4426ffa
Ek4nsh Tiwari <xyz.colian@gmail.com>
2026-04-18 10:35:30 +0530

    Update API key in proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
 
   try {
     // Construct the target URL
-    const targetUrl = `http://api.subhxcosmo.in/api?key=${key}&type=${type}&term=${term}`;
+    const targetUrl = `http://api.subhxcosmo.in/api?key=suryansh&type=${type}&term=${term}`;
 
     // Fetch data from the external API
     const response = await fetch(targetUrl);
